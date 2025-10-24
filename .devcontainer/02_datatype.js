749b90479a8b0493f3a8100b3824e9938d3f44be
swagatikapriyadarshinisahoo033-ship-it <swagatikapriyadarshinisahoo033@gmail.com>
2025-10-24 23:42:54 +0530

    Update 02_datatype.js

--- .devcontainer/02_datatype.js
+++ .devcontainer/02_datatype.js
@@ -1,1 +1,31 @@
+"user strict"//treat js code as newer version
+
+//alert(3+3) //getting error because it's a nodejs not a brouser
+
+//console.log(3+3);console.log("swag")//readebility in not good when you have a next line why you write in same 
+// line with semicolon semicolon is not good in js
+
+console.log(3+3)//readbelity is high
+console.log("swag")//readbelity is high
+//  datatype
+let name="swag"
+let age=19
+let islogedin=true
+
+/* string=>" "
+number=>range is 2 to the power53
+boolean=>true,false
+null=>it's a standalone value
+undefined =>just nothing
+symbol=>unique
+*/
+//  object=>to show the variable name
+console.log(typeof("hitesh"))//string
+console.log(typeof age)//number
+console.log(typeof null)//object
+console.log(typeof undefined)//undefiend
+console.log(typeof islogedin)//boolean
+
+
+
 
