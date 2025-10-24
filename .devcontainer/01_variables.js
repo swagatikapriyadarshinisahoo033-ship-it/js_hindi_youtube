dc12f9e5f4b42e3dbede68b0bcb1d64badb52a82
swagatikapriyadarshinisahoo033-ship-it <swagatikapriyadarshinisahoo033@gmail.com>
2025-10-24 23:06:08 +0530

    Update 01_variables.js

--- .devcontainer/01_variables.js
+++ .devcontainer/01_variables.js
@@ -1,1 +1,16 @@
+const accountId=144553
+let accountEmail="swagatika@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+let accountstate;
+// accountId=2 //not allowed in js
+/*
+don't use var
+beacuse of issue in block scope and functional scope
+*/
+accountEmail="sam@gmail.com"
+accountpassword="2121212"
+accountcity="cuttack"
+console.log(accountId);
+console.table([accountEmail,accountId,accountpassword,accountcity,accountstate])
 
