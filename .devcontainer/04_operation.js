cf19c681239c438685aeda79da6b55507f1e059e
swagatikapriyadarshinisahoo033-ship-it <swagatikapriyadarshinisahoo033@gmail.com>
2025-10-25 19:08:21 +0530

    Update 04_operation.js

--- .devcontainer/04_operation.js
+++ .devcontainer/04_operation.js
@@ -1,1 +1,47 @@
 
+let s=3
+let s2=-s
+// console.log(s2)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2**2);
+// console.log(2%4);
+
+//let str1="hello"
+//let str2="swag"
+////let str3=str1+str2
+// console.log(str3);
+
+/*it's called messy code
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log("1"+"2");
+console.log(3 + 4 * 5 % 6);
+*/
+/*
+console.log(true)//true
+console.log(+true)//1
+console.log(true+)//error
+console.log(+" ")//0
+*/
+
+/*
+let gamecounter=100
+gamecounter++;//101 post fix
+++gamecounter;//101 pre  fix
+console.log(gamecounter)
+*/
+let x = 3;
+const y = x++;
+console.log(x,y)
+// x is 4; y is 3
+
+let x2 = 3;
+const y2 = ++x2;
+console.log(x2,y2)
+// x2 is 4n; y2 is 3n
+
+
