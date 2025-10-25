1a26fcc3cff686fcf2d43d8516a86c94a214729f
swagatikapriyadarshinisahoo033-ship-it <swagatikapriyadarshinisahoo033@gmail.com>
2025-10-25 19:30:54 +0530

    Update 05_comparision.js

--- .devcontainer/05_comparision.js
+++ .devcontainer/05_comparision.js
@@ -1,1 +1,26 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1)
+// console.log("2"==1)
+// console.log("2">=1)
+/*
+console.log(null==0)//false
+console.log(null>0)//false
+console.log(null>=0)//true
+*/ 
+/* the reason is equal opearot and comparision opeartor work differnetly
+comparision convert null to a number treating is as 0
+that's why (3) null>=0 true and (1)null>0 false */
+//  console.log(undefined==0)//false
+//  console.log(undefined>0)//false
+//  console.log(undefined<0)//false
+
+
+ //=== its' check strictly mean it's check not only value also check the datatype
+console.log("2"==2)//it's retrun true
+console.log("2"===2)//it's check the strictly way means it's also check the datatype
 
